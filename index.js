ae8b5518ada8f3c2b144a644aefb9263727bbe01
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-26 17:24:54 +0100

    Bonus

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello Boolean") 
+//console.log("Hello Boolean")
+
+const name = process.argv[2]
+
+if (name) {
+    console.log("Hello " + name)
+} else {
+    console.log("Hello World")
+}
